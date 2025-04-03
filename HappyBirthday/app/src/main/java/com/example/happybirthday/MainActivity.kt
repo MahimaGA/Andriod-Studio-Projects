b44c9514a42addd0f800e6b8791d478ee0d1a9db
unknown <mahimaagarwal285@gmail.com>
2025-04-03 14:00:30 +0545

    Update to Extension

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -20,6 +20,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,9 +38,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingImage(
-                        message = "Happy Birthday DMA MODULE!",
-                        from = "From Everyone",
+//                    GreetingImage(
+//                        message = "Happy Birthday DMA MODULE!",
+//                        from = "From Everyone",
+//                        modifier = Modifier.padding(8.dp)
+//                    )
+
+                    GreetingImageChristmas(
+                        message = "Merry Christmas",
+                        from = "From Leeds Beckett Computing",
                         modifier = Modifier.padding(8.dp)
                     )
                 }
@@ -49,6 +57,58 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 500.dp),
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+        ) {
+            Text(
+                text = message,
+                fontSize = 80.sp,
+                lineHeight = 60.sp,
+                textAlign = TextAlign.Center,
+                color = Color.White,
+                fontFamily = FontFamily.Cursive,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = from,
+                fontSize = 26.sp,
+                color = Color.White,
+                fontFamily = FontFamily.Cursive,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(alignment = Alignment.CenterHorizontally)
+                    .padding(top = 8.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+//        GreetingImage(
+//            message = stringResource(R.string.happy_birthday_dma_module),
+//            from = stringResource(R.string.signature_text)
+//        )
+
+            GreetingImageChristmas(
+                message = stringResource(R.string.merry_christmas_dma_module),
+                from = stringResource(R.string.signature_text)
+            )
+        }
+}
+
+
+@Composable
+fun GreetingTextChristmas(message: String, from: String, modifier: Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
         modifier = modifier
@@ -64,24 +124,34 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
             text = from,
             fontSize = 36.sp,
             color = Color.White,
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .align(alignment = Alignment.End)
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
         )
     }
 }
-
-@Preview(showBackground = true)
 @Composable
-fun BirthdayCardPreview() {
-    HappyBirthdayTheme {
-        GreetingImage(
-            message = stringResource(R.string.happy_birthday_dma_module),
-            from = stringResource(R.string.signature_text)
+fun GreetingImageChristmas(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.christmas)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F,
+            modifier = Modifier
+                .background(Color.DarkGray)
+
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
         )
     }
 }
-
 @Composable
 fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.androidparty)
