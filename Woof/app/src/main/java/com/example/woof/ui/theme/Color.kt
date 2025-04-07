53a16a5f2a654200e81d4614094304284c488956
unknown <mahimaagarwal285@gmail.com>
2025-04-07 19:35:40 +0545

    Add Color Scheme

--- Woof/app/src/main/java/com/example/woof/ui/theme/Color.kt
+++ Woof/app/src/main/java/com/example/woof/ui/theme/Color.kt
@@ -17,10 +17,12 @@ package com.example.woof.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val md_theme_light_background = Color(0xFFFFFBFF)
+val md_theme_light_surface = Color(0xFFFFFBFF)
+val md_theme_light_onSurface = Color(0xFF646153)
+val md_theme_light_surfaceContainerHighest = Color(0xFFECE4D4)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val md_theme_dark_background = Color(0xFF201C14)
+val md_theme_dark_surface = Color(0xFF201C14)
+val md_theme_dark_onSurface = Color(0xFFBDB7A5)
+val md_theme_dark_surfaceContainerHighest = Color(0xFF4B4739)
