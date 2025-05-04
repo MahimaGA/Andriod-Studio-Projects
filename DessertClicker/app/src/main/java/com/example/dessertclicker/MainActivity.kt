4a9c62bf6fdcbd167f30bfcb0354c48aab5fe686
unknown <mahimaagarwal285@gmail.com>
2025-05-04 19:00:56 +0545

    Add logging onCreate(), onStart(), onResume()

--- DessertClicker/app/src/main/java/com/example/dessertclicker/MainActivity.kt
+++ DessertClicker/app/src/main/java/com/example/dessertclicker/MainActivity.kt
@@ -20,6 +20,7 @@ import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -63,9 +64,22 @@ import com.example.dessertclicker.data.Datasource
 import com.example.dessertclicker.model.Dessert
 import com.example.dessertclicker.ui.theme.DessertClickerTheme
 
+private const val TAG = "MainActivity"
+
 class MainActivity : ComponentActivity() {
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume Called")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate Called")
         setContent {
             DessertClickerTheme {
                 // A surface container using the 'background' color from the theme
@@ -320,3 +334,4 @@ fun MyDessertClickerAppPreview() {
         DessertClickerApp(listOf(Dessert(R.drawable.cupcake, 5, 0)))
     }
 }
+
