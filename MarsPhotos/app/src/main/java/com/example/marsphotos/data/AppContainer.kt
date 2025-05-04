89f5e17f85ab6b8040c1bfdd72ecfea942d4890b
unknown <mahimaagarwal285@gmail.com>
2025-05-04 23:33:50 +0545

    Add MarsPhotosRepository and update AppContainer

--- MarsPhotos/app/src/main/java/com/example/marsphotos/data/AppContainer.kt
+++ MarsPhotos/app/src/main/java/com/example/marsphotos/data/AppContainer.kt
@@ -1,2 +1,23 @@
 package com.example.marsphotos.data
 
+import com.example.marsphotos.network.MarsApiService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
+
+interface AppContainer {
+}
+
+class DefaultAppContainer : AppContainer {
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(BASE_URL)
+        .build()
+
+    private val retrofitService : MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
