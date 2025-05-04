1d852bf8e9a6ea77cec57359e7b475532b47a195
unknown <mahimaagarwal285@gmail.com>
2025-05-05 01:15:40 +0545

    Create dataclass DessertUiState

---
+++ DessertClicker/app/src/main/java/com/example/dessertclicker/data/DessertUiState.kt
@@ -0,0 +1,9 @@
+package com.example.dessertclicker.data
+
+data class DessertUiState(
+    val revenue: Int,
+    val dessertsSold: Int,
+    val currentDessertIndex: Int,
+    val currentDessertPrice: Int,
+    val currentDessertImageId: Int
+)
